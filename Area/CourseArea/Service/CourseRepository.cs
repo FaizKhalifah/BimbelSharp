9bc5dca1a67fe757e955bfc982e52f170ae0e0c6
Faiz Khalifah <faiz_doc@outlook.com>
2025-02-11 13:59:11 +0700

    feat: course repository and irepository

---
+++ Area/CourseArea/Service/CourseRepository.cs
@@ -0,0 +1,47 @@
+﻿using Bimbelsharp.Data;
+using Bimbelsharp.Data.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bimbelsharp.Area.CourseArea.Service
+{
+    public class CourseRepository:ICourseRepository
+    {
+        public CourseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        private readonly ApplicationDbContext _context;
+
+        public async Task<IEnumerable<Course>> GetAllCoursesAsync()
+        {
+            return await _context.Courses.ToListAsync();
+        }
+
+        public async Task<Course> GetCourseByIdAsync(int id)
+        {
+            return await _context.Courses.FindAsync(id);
+        }
+
+        public async Task<Course> AddCourseAsync(Course course)
+        {
+            await _context.Courses.AddAsync(course);
+            await _context.SaveChangesAsync();
+            return course;
+        }
+
+        public async Task<bool> UpdateCourseAsync(Course course)
+        {
+            _context.Courses.Update(course);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> DeleteCourseAsync(int id)
+        {
+            var course = await _context.Courses.FindAsync(id);
+            if (course == null) return false;
+
+            _context.Courses.Remove(course);
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
