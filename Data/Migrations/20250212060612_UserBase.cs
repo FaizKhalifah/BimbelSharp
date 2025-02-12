1ec9cfe60ec8e762c5ce1977d6a6738c42c28a89
Faiz Khalifah <faiz_doc@outlook.com>
2025-02-12 13:32:00 +0700

    feat: user base model and jwt helper utilities

---
+++ Data/Migrations/20250212060612_UserBase.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bimbelsharp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserBase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Teachers",
+                newName: "HashedPassword");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Students",
+                newName: "HashedPassword");
+
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Admins",
+                newName: "HashedPassword");
+
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Admins",
+                newName: "FullName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "Teachers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                table: "Teachers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Students",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "Students",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                table: "Students",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Admins",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                table: "Admins",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Admins");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Admins");
+
+            migrationBuilder.RenameColumn(
+                name: "HashedPassword",
+                table: "Teachers",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "HashedPassword",
+                table: "Students",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "HashedPassword",
+                table: "Admins",
+                newName: "Username");
+
+            migrationBuilder.RenameColumn(
+                name: "FullName",
+                table: "Admins",
+                newName: "Password");
+        }
+    }
+}
---
+++ Data/Model/User.cs
@@ -0,0 +1,17 @@
+﻿namespace Bimbelsharp.Data.Model
+{
+    public class UserBase:BaseModel
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string HashedPassword { get; set; }
+        public Role Role { get; set; }
+    }
+
+    public enum Role
+    {
+        Admin,
+        Teacher,
+        Student
+    }
+}
