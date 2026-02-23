ee968a3f3d4ea8af7cb0b3e1266486a0be5c2466
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2026-02-23 08:16:37 +0700

    feat: index utils

---
+++ src/utils/index.js
@@ -0,0 +1,47 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+import {APP_SECRET} from "../config/index.js";
+
+async function generateSalt() {
+    return await bcrypt.genSalt();
+}
+
+async function generatePassword(password,salt) {
+    return await bcrypt.hash(password,salt);
+}
+
+async function validatePassword(inputPassword, savedPassword, salt) {
+    const generatedPassword = await generatePassword(inputPassword,salt);
+    return generatedPassword === savedPassword;
+}
+
+async function validateSignature(params) {
+    try {
+        const signature = req.get("Authorization");
+        console.log(signature);
+        const payload = await jwt.verify(signature.split(" ")[1], APP_SECRET);
+        req.user = payload;
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+function formateData(data){
+    if(data){
+        return {data};
+    }
+    else{
+        throw new Error("Data not found!");
+    }
+}
+
+export default{
+    generatePassword,
+    generateSalt,
+    validatePassword,
+    validateSignature,
+    formateData
+}
