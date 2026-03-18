d09ba76ce74315c4038dd5ddd7a28bc8466f360a
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2026-03-18 17:46:11 +0700

    fix : calling base repository class in other repositories

--- src/repositories/MaterialRepository.js
+++ src/repositories/MaterialRepository.js
@@ -1,29 +1,9 @@
 import Material from "../models/Material.js";
+import BaseRepository from "./BaseRepository.js";
 
-class MaterialRepository{
-    async createMaterial(data){
-        const material = new Material(data);
-        return await material.save();
-    }
-
-    async getAllMaterial(){
-        const materials = await Material.find();
-        return materials;
-    }
-
-    async getMaterialById(id){
-        const material = await Material.findById(id);
-        return material;
-    }
-
-    async updateMaterial(data,id){
-        const updatedMaterial = await Material.findByIdAndUpdate(id,data,{new:true});
-        return updatedMaterial;
-    }
-
-    async deleteMaterial(id){
-        const deletedMaterial = await Material.findByIdAndDelete(id);
-        return deletedMaterial;
+class MaterialRepository extends BaseRepository{
+    constructor(){
+        super(Material);
     }
 }
 
--- src/repositories/courseRepository.js
+++ src/repositories/courseRepository.js
@@ -1,31 +1,11 @@
 import Course from "../models/Course.js";
+import BaseRepository from "./BaseRepository.js";
 
-class CourseRepository{
-
-    async createCourse(data) {
-        const course = new Course(data);
-        return await course.save();
-    }
-
-    async getAllCourse() {
-        const courses = await Course.find();
-        return courses;
-    }
-
-    async getCourseById(id) {
-        const course = await Course.findById(id);
-        return course;
-    }
-
-    async updateCourse(id, data){
-        return await Course.findByIdAndUpdate(id,data,{new:true});
-    }
-
-    async deleteCourse(id) {
-        return await Course.findByIdAndDelete(id);
+class CourseRepository extends BaseRepository{
+    constructor(){
+        super(Course);
     }
 }
-
 export default{
     CourseRepository
 }
