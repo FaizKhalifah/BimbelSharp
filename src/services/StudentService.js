d4b113e9398270706902d5c47a88905d9ff8838a
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2026-03-19 14:11:10 +0700

    feat: student service

--- src/services/StudentService.js
+++ src/services/StudentService.js
@@ -10,7 +10,11 @@ class StudentService{
 
     async createStudent(studentData){
         try{
-            const createStudentResult = await this.repository.createStudent(studentData);
+            const isAvailable = await this.repository.findById(id);
+            if(isAvailable){
+                throw new APIError("Student already exist");
+            }
+            const createStudentResult = await this.repository.create(studentData);
             return formateData(createStudentResult);
         }catch(err){
             throw new APIError("data not found");
@@ -18,10 +22,46 @@ class StudentService{
     }
 
     async getAllStudent(){
-
+        try{
+            const students = await this.repository.findAll();
+            return formateData(students);
+        }catch(err){
+            throw new APIError("data not found");
+        }
     }
 
     async getStudentById(id){
+        try{
+            const student = await this.repository.findById(id);
+            return formateData(student);
+        }catch(err){
+            throw new APIError("student not found");
+        }
+    }
 
+    async updateStudent(id,data){
+        try{
+            const isAvailable = await this.repository.findById(id);
+            if(!isAvailable){
+                throw new APIError("Student not found");
+            }
+            const updatedStudent = await this.repository.update(id,data);
+            return formateData(updatedStudent);
+        }catch(err){
+            throw new APIError("error updating data");
+        }
+    }
+
+    async deleteStudent(id){
+        try{
+            const isAvailable = await this.repository.findById(id);
+            if(!isAvailable){
+                throw new APIError("Student not found");
+            }
+            const deletedStudent = await this.repository.delete(id);
+            return formateData(deletedStudent);
+        }catch(err){
+            throw new APIError("Error deleting data");
+        }
     }
 }
