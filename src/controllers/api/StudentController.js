00a283c741902eb00df15f54eef7ed06bafc5858
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2026-03-20 09:46:07 +0700

    feat: student controller

--- src/controllers/api/StudentController.js
+++ src/controllers/api/StudentController.js
@@ -8,7 +8,52 @@ class StudentController{
             const requestDTO = studentDTO.createStudentDTO(req.body);
             studentValidators.createStudentSchema.parse(requestDTO);
             const student = await StudentService.createStudent(requestDTO);
-            return res.status(201).json(student);
+            res.status(201).json(student);
+        }catch(err){
+            next(err);
+        }
+    }
+
+    async getAll(req,res,next){
+        try{
+            const students = await StudentService.getAllStudent();
+            res.status(201).json(students);
+        }catch(err){
+            next(err)
+        }
+    }
+
+    async getById(req,res,next){
+        try{
+            const studentID = req.params.id;
+            studentValidators.objectIdSchema.parse(studentID);
+            const student = await StudentService.getStudentById(studentID);
+            res.status(201).json(student);
+        }catch(err){
+            next(err);
+        }
+    }
+
+    async update(req,res,next){
+        try{
+            const studentID = req.params.id;
+            studentValidators.objectIdSchema.parse(studentID);
+
+            const dto = studentDTO.updateStudentDTO(req.body);
+            studentValidators.updateStudentSchema.parse(dto);
+            const updateResult = await StudentService.updateStudent(studentID,dto);
+            res.status(201).json(updateResult);
+        }catch(err){
+            next(err);
+        }
+    }
+
+    async delete(req,res,next){
+        try{
+            const studentID = req.params.id;
+            studentValidators.objectIdSchema.parse(studentID);
+            const deleteResult = await StudentService.deleteStudent(studentID);
+            res.json(201).json(deleteResult);
         }catch(err){
             next(err);
         }
