b4b240372a09fdda1719fd37e08927403d7b3e64
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2026-03-28 13:36:27 +0700

    feat: teacher controller

---
+++ src/controllers/api/teacherController.js
@@ -0,0 +1,62 @@
+import teacherService from "../../services/TeacherService.js";
+import teacherDTO from "../../DTO/teacherDTO.js";
+import teacherValidator from "../../validators/teacherValidators.js";
+
+class TeacherController{
+    async create(req,res,next){
+            try{
+                const requestDTO =teacherDTO.createTeacherDTO(req.body);
+                teacherValidator.createTeacherSchema.parse(requestDTO);
+                const teacher = await teacherService.createTeacher(requestDTO);
+                res.status(201).json(teacher);
+            }catch(err){
+                next(err);
+            }
+        }
+    
+        async getAll(req,res,next){
+            try{
+                const teachers = await teacherService.getAllTeachers();
+                res.status(201).json(teachers);
+            }catch(err){
+                next(err)
+            }
+        }
+    
+        async getById(req,res,next){
+            try{
+                const teacherID = req.params.id;
+                teacherValidator.objectIdSchema.parse(teacherID);
+                const teacher = await teacherService.getTeacherById(teacher);
+                res.status(201).json(teacher);
+            }catch(err){
+                next(err);
+            }
+        }
+    
+        async update(req,res,next){
+            try{
+                const teacherID = req.params.id;
+                teacherValidator.objectIdSchema.parse(teacherID);
+                const dto = teacherDTO.updateTeacherDTO(req.body);
+                teacherValidator.updateTeacherSchema.parse(dto);
+                const updateResult = await teacherService.updateTeacher(teacherID,dto);
+                res.status(201).json(updateResult);
+            }catch(err){
+                next(err);
+            }
+        }
+    
+        async delete(req,res,next){
+            try{
+                const teacherID = req.params.id;
+                teacherValidator.objectIdSchema.parse(teacherID);
+                const deleteResult = await teacherService.deleteTeacher(teacherID);
+                res.json(201).json(deleteResult);
+            }catch(err){
+                next(err);
+            }
+        }
+}
+
+export default new TeacherController();
