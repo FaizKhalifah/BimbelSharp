71ab600c87925f98ae0bd2edca8727777d077b17
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2026-04-21 12:29:33 +0700

    feat: course routes

--- src/routes/api/apiIndexRoutes.js
+++ src/routes/api/apiIndexRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import studentRouter from "./studentRoutes.js";
 import teacherRouter from "./teacherRoutes.js";
+import courseRouter from "./courseRoutes.js";
 
 const apiRouter = express.Router();
 
 apiRouter.use("/api/students/",studentRouter);
-apiRouter.use("/api/teachers/",teacherRouter)
+apiRouter.use("/api/teachers/",teacherRouter);
+apiRouter.use("/api/courses/",courseRouter);
 
 export default apiRouter;
