e11ac51b28a4aa56f356f4346cf4e66bcdbac24a
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2026-05-02 18:01:09 +0700

    feat: student index page

--- src/routes/web/webIndexRoutes.js
+++ src/routes/web/webIndexRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import teacherViewRoutes from "./teacherViewRoutes.js";
+import studentViewRoutes from "./studentViewRoutes.js";
 
 const viewRouter = express.Router();
 
@@ -7,5 +8,6 @@ viewRouter.get("/",(req,res)=>{
     res.render("pages/index",{ title: 'Bimbelsharp' })
 });
 viewRouter.use("/teachers",teacherViewRoutes);
+viewRouter.use("/students",studentViewRoutes);
 
 export default viewRouter;
