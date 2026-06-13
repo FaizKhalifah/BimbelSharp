696f645b18d0b0a651020d56190faf934f5d6164
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2026-06-13 21:30:11 +0700

    feat: material service

--- src/repositories/MaterialRepository.js
+++ src/repositories/MaterialRepository.js
@@ -5,6 +5,15 @@ class MaterialRepository extends BaseRepository{
     constructor(){
         super(Material);
     }
+
+    async getAllWithPagination(skip, limit) {
+        return this.model
+        .find()
+        .sort({ createdAt: -1 })
+        .skip(skip)
+        .limit(limit)
+        .lean();
+    }
 }
 
 export default MaterialRepository;
--- src/services/materialService.js
+++ src/services/materialService.js
@@ -22,6 +22,45 @@ class MaterialService{
 
     }
 
+    async getAllMaterial({ page = 1, limit = 10 } = {}){
+        const skip = (page - 1) * limit;
+
+        const materials = await this.MaterialRepository.getAllWithPagination(skip,limit);
+
+        return formateData({
+            page,
+            limit,
+            data: materials
+        });
+
+    }
+
+    async getMaterialById(id){
+        const material = await this.MaterialRepository.findById(id);
+        if(!material){
+            throw new BadRequestError("Material not found");
+        }
+        return formateData(material);
+    }
+
+    async updateMaterial(id,updateData){
+        const isAvailable = await this.MaterialRepository.findById(id);
+        if(!isAvailable){
+             throw new BadRequestError("Material not found");
+        }
+        const updateResult = await this.MaterialRepository(id,updateData);
+        return formateData(updateResult);
+    }
+
+    async deleteMaterial(id){
+        const isAvailable = await this.MaterialRepository.findById(id);
+        if(!isAvailable){
+             throw new BadRequestError("Material not found");
+        }
+        const deleteResult = await this.MaterialRepository.delete(id);
+        return formateData(deleteResult);
+    }
+
 }
 
 export default new MaterialService();
